d1a265643d9b6aace5a2c4eb23f8666a691cfc35
Wang-yaole <37959954+Wang-yaole@users.noreply.github.com>
2020-02-12 13:49:08 +0800

    init

---
+++ test-site/scripts/main.js
@@ -0,0 +1,33 @@
+let myImage=document.querySelector('img')
+myImage.onclick=function() {
+    let mySrc=myImage.getAttribute('src')
+    if(mySrc=='images/test.jpg'){
+        myImage.setAttribute('src','images/test2.jpg')
+    }
+    else {
+        myImage.setAttribute('src','images/test.jpg')
+    }
+}
+let myButton=document.querySelector('button')
+let myHeading=document.querySelector('h1')
+
+function setUserName() {
+    let myName=prompt('请输入你的名字：')
+    if(!myName || myName===null) {
+        setUserName()
+    } else {
+    localStorage.setItem('name',myName)
+    myHeading.textContent='Mozilla 真牛哔，'+myName
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName()
+} else {
+    let storedName=localStorage.getItem('name')
+    myHeading.textContent='Mozilla 真牛哔，'+storedName
+}
+
+myButton.onclick=function() {
+    setUserName();
+}
